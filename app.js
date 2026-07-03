cf926c1fcbe0832b862274b853e66cddd734e068
Azizbek2343 <nasritdinovAzizbek001@gmail.com>
2026-07-03 08:21:19 +0500

    Full finished

--- app.js
+++ app.js
@@ -15,13 +15,77 @@ const result = `Qadrli ${ism}. Siz ${tugilganYil} tug'ilgansiz. Va siz ${oy} oy,
 
 alert (result);
 */
+ 
 
+// -------------------------- 2 --------------------------
+/*
 const ism = prompt("Ism kiriting");
 
-const ismlar = [Azizbek, Shuhrat, Foziljon];
+const ismlar = ['Azizbek', 'Shuhrat', 'Foziljon'];
 
 if(ismlar.includes(ism)) {
     alert(`Ha ${ism} bor`);
-} else{
+} else {
     alert(`Afsus ${ism} yuq`);
+}
+*/
+
+
+// -------------------------- 3 --------------------------
+
+// const ismlar = ['Aziz', 'Shuhrat', 'Fozil'];
+
+// for / while => loop 
+
+/* 
+for (let i = 0; i < ismlar.length; i++) {
+    let result = ismlar[i] + 'bek'
+    console.log(result);
+}
+*/
+
+/*
+let i = 0;
+
+while (i < ismlar.length) {
+    let result = ismlar[i] + 'bek'
+    console.log(result);
+    i++
+}
+*/
+
+
+// -------------------------- 4 --------------------------
+/*
+const viloyatNomi = prompt("Qaysi viloyat kerak?");
+
+ switch (viloyatNomi) {
+    case "Farg'ona":
+        alert("Farg'ona viloyatida 2 mln aholi bor");
+        break;
+    case "Andijon":
+        alert("Andijon viloyatida 1.5 mln aholi bor");
+        break;
+    case "Namangan":
+        alert("Namangan viloyatida 2.5 mln aholi bor");
+        break;
+    default:
+        alert(`Hozircha bazida ${viloyatNomi} viloyati mavjud emas.`);
+ }
+*/
+
+
+// -------------------------- 5 --------------------------
+const belgilar = prompt("Nimadir yozing...");
+
+let meyor = 20;
+
+if (belgilar.length > meyor) {
+    alert(
+        `Siz ${meyor} ta belgi kiritishingiz mumkin edi, lekin hozirda siz ${belgilar.length} ta belgi kiritdingiz. Bu meyoridan ${belgilar.length - meyor} ta ko'p.`
+    );
+} else{
+    alert(
+        `Siz ${meyor} ta belgi kiritishingiz mumkin va hozirda siz ${belgilar.length} ta belgi kiritdingiz. Yana ${meyor - belgilar.length} ta belgi kiritishingiz mumkin.`
+    );
 }
